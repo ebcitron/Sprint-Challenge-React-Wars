b4b5fbfb24eeb837ecb451a21da3c688fbd47063
Eli Citron <ebcitron@gmail.com>
2019-03-29 12:34:41 -0500

    StyledF

--- starwars/src/components/Char.js
+++ starwars/src/components/Char.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './StarWars.css';
 
 const Char = props => {
     return (
         <div className=' char '>
             
-            <h3>{props.charName.name}</h3>
+            <h2>{props.charName.name}</h2>
+            <h4>Birth Year: {props.charName.birth_year}</h4>
+
         </div >
     );
 }
--- starwars/src/components/StarWarsChars.js
+++ starwars/src/components/StarWarsChars.js
@@ -5,7 +5,6 @@ const StarWarsChars = props => {
     console.log(props.characters.name);
   return (
       <div className="chars">
-          <h1>Star Wars Characters</h1>
       {props.characters.map(char => {
           return (
               <div>
